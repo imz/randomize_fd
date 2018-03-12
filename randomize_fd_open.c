ff9b146138a5821b3e702fe77cf8be19430a0ae1
Ivan Zakharyaschev <imz@altlinux.org>
2018-03-12 18:15:41 +0300

    randomize_fd() implemented and used in open()

--- randomize_fd_open.c
+++ randomize_fd_open.c
@@ -1,15 +1,34 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 
-#include <stdio.h>
+#include <unistd.h>
+#define RANDOMIZE_FD_STEP 6
+
+static int dup_many_times_and_close (const int old_result, const unsigned int times)
+{
+  if (times == 0 || old_result < 0)
+    return old_result;
+
+  const int final_result = dup_many_times_and_close (dup (old_result), times - 1);
+  if (close (old_result))
+    ;
+  return final_result;
+}
+
+static int randomize_fd (const int old_result)
+{
+  static char phase; /* the initial value is not very important */
+
+  if (old_result < 0)
+    return 0;
+
+  return dup_many_times_and_close (old_result, RANDOMIZE_FD_STEP * (1 + (phase++ & 0x7)));
+}
 
 typedef int (*orig_open_f_type)(const char *pathname, int flags);
 
 int open (const char *pathname, int flags, ...)
 {
-  /* Some evil injected code goes here. */
-  printf ("The victim used open(...) to access '%s'!!!\n",pathname); //remember to include stdio.h!
-
   const orig_open_f_type orig_open = (orig_open_f_type) dlsym (RTLD_NEXT, "open");
-  return orig_open (pathname, flags);
+  return randomize_fd (orig_open (pathname, flags));
 }
