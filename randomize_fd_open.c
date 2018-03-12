0ac34d8f03bb4f1f781e2cbbae4a091597226609
Ivan Zakharyaschev <imz@altlinux.org>
2018-03-12 15:56:53 +0300

    *.c: indent with 2 spaces and other GNU C style adaptations

--- randomize_fd_open.c
+++ randomize_fd_open.c
@@ -5,12 +5,11 @@
 
 typedef int (*orig_open_f_type)(const char *pathname, int flags);
 
-int open(const char *pathname, int flags, ...)
+int open (const char *pathname, int flags, ...)
 {
-    /* Some evil injected code goes here. */
-    printf("The victim used open(...) to access '%s'!!!\n",pathname); //remember to include stdio.h!
+  /* Some evil injected code goes here. */
+  printf ("The victim used open(...) to access '%s'!!!\n",pathname); //remember to include stdio.h!
 
-    orig_open_f_type orig_open;
-    orig_open = (orig_open_f_type)dlsym(RTLD_NEXT,"open");
-    return orig_open(pathname,flags);
+  const orig_open_f_type orig_open = (orig_open_f_type) dlsym (RTLD_NEXT, "open");
+  return orig_open (pathname, flags);
 }
